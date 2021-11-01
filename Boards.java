2cb776f2aa8eb1d563a11be2d8e8687ccd14ab9a
Daniel M <93309254+DanielTestGit@users.noreply.github.com>
2021-11-01 17:14:47 +0100

    Add files via upload
    Skapat fem spelplaner och en randommetod 
    i Boardsclassen, som går att använda för 
    att testa skriva ut en slumpmässig spelplan genom Main till terminalfönstret.

---
+++ Boards.java
@@ -0,0 +1,96 @@
+package BattleshipCode;
+
+import java.util.Random;
+
+public class Boards {
+
+        //Constructor
+        public Boards(){}
+
+        //Variables
+        private static final Random random = new Random();
+        private static final int randomBoard = random.nextInt(5)+1;
+
+        //Method for getting a random board
+        public int[][] getRandomBoard(){
+                var chosenBoard = new int[10][10];
+                switch (randomBoard){
+                        case 1:
+                                chosenBoard = board1;
+                                break;
+                        case 2:
+                                chosenBoard = board2;
+                                break;
+                        case 3:
+                                chosenBoard = board3;
+                                break;
+                        case 4:
+                                chosenBoard = board4;
+                                break;
+                        case 5:
+                                chosenBoard = board5;
+                                break;
+                } return chosenBoard;
+
+        }
+
+        //2-D arrays with five different ship placements
+        //Symbol meaning: 0=Water, 5=Aircraft Carrier, 4=Battleship, 3=Cruiser, 2=Submarine
+        //Length*Number of each ship: H=5*1, B=4*2, C=3*3, S=2*4 (All ships are 1 in Width)
+
+        private int[][] board1 =  {{0,0,0,0,0,0,0,0,0,4,},
+                                   {0,5,5,5,5,5,0,0,0,4,},
+                                   {0,0,0,0,0,0,0,0,0,4,},
+                                   {0,2,0,0,0,3,3,3,0,4,},
+                                   {0,2,0,2,0,0,0,0,0,0,},
+                                   {0,0,0,2,0,0,2,2,0,0,},
+                                   {0,2,0,0,0,0,0,0,0,4,},
+                                   {0,2,0,0,0,3,3,3,0,4,},
+                                   {0,0,0,0,0,0,0,0,0,4,},
+                                   {3,3,3,0,0,0,0,0,0,4,}};
+
+        private int[][] board2 =  {{0,0,0,5,0,0,0,0,4,0,},
+                                   {0,3,0,5,0,0,0,0,4,0,},
+                                   {0,3,0,5,0,2,2,0,4,0,},
+                                   {0,3,0,5,0,0,0,0,4,0,},
+                                   {0,0,0,5,0,0,0,0,0,0,},
+                                   {0,0,0,0,0,0,0,3,3,3,},
+                                   {0,4,0,2,2,0,0,0,0,0,},
+                                   {0,4,0,0,0,0,0,2,0,3,},
+                                   {0,4,0,0,2,2,0,2,0,3,},
+                                   {0,4,0,0,0,0,0,0,0,3,}};
+
+        private int[][] board3 =  {{0,3,3,3,0,0,4,4,4,4,},
+                                   {0,0,0,0,0,0,0,0,0,0,},
+                                   {0,4,4,4,4,0,0,0,0,5,},
+                                   {0,0,0,0,0,0,0,0,0,5,},
+                                   {0,2,2,0,2,0,0,0,0,5,},
+                                   {0,0,0,0,2,0,0,0,0,5,},
+                                   {0,0,3,0,0,0,2,2,0,5,},
+                                   {0,0,3,0,0,0,0,0,0,0,},
+                                   {0,0,3,0,3,3,3,0,2,0,},
+                                   {0,0,0,0,0,0,0,0,2,0,}};
+
+        private int[][] board4 =  {{0,0,0,0,0,0,0,0,0,0,},
+                                   {0,3,3,3,0,0,0,3,3,3,},
+                                   {0,0,0,0,0,0,0,0,0,0,},
+                                   {0,2,2,0,5,0,2,2,0,0,},
+                                   {0,0,0,0,5,0,0,0,0,0,},
+                                   {3,3,3,0,5,0,0,0,4,0,},
+                                   {0,0,0,0,5,0,2,0,4,0,},
+                                   {0,2,2,0,5,0,2,0,4,0,},
+                                   {0,0,0,0,0,0,0,0,4,0,},
+                                   {0,0,0,4,4,4,4,0,0,0,}};
+
+        private int[][] board5 =  {{0,0,0,0,0,0,0,0,4,0,},
+                                   {0,0,0,0,0,0,0,0,4,0,},
+                                   {3,3,3,0,0,0,2,0,4,0,},
+                                   {0,0,0,0,5,0,2,0,4,0,},
+                                   {0,0,3,0,5,0,0,0,0,0,},
+                                   {4,0,3,0,5,0,0,0,0,0,},
+                                   {4,0,3,0,5,0,2,2,0,0,},
+                                   {4,0,0,0,5,0,0,0,0,2,},
+                                   {4,0,0,0,0,0,2,0,0,2,},
+                                   {0,0,3,3,3,0,2,0,0,0,}};
+
+}
---
+++ Main.java
@@ -0,0 +1,22 @@
+package BattleshipCode;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Create an instance of the Boards class
+        Boards board = new Boards();
+
+        //Stores the random chosen board in the 2D-array
+        var selectedBoard = board.getRandomBoard();
+
+        //Loops through the selected 2D-array(board) and prints it out in the Terminal
+        for (int x = 0; x < selectedBoard.length; x++) {
+            for (int y = 0; y < selectedBoard.length; y++) {
+                System.out.print(selectedBoard[x][y] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
